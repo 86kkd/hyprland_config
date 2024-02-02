db7b81d4697003b89985d8ff58abb1645f5663b5
86kkd <2973720373@qq.com>
2024-02-02 22:56:51 +0800

    init

---
+++ scripts/WaybarCava.sh
@@ -0,0 +1,33 @@
+# #! /bin/bash
+## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+
+# Not my own work. This was added through Github PR. Credit to original author
+
+#----- Optimized bars animation without much CPU usage increase --------
+bar="▁▂▃▄▅▆█▇"
+dict="s/;//g"
+
+# Calculate the length of the bar outside the loop
+bar_length=${#bar}
+
+# Create dictionary to replace char with bar
+for ((i = 0; i < bar_length; i++)); do
+    dict+=";s/$i/${bar:$i:1}/g"
+done
+
+# Create cava config
+config_file="/tmp/bar_cava_config"
+cat >"$config_file" <<EOF
+[general]
+bars = 11
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+EOF
+
+# Read stdout from cava and perform substitution in a single sed command
+cava -p "$config_file" | sed -u "$dict"
+# cava
